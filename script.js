b79c632e806ccf957c3c03354a6269d2d56fe083
Taiwo <oyeniyi4real@gmail.com>
2019-09-02 10:02:59 +0100

    making it work

--- script.js
+++ script.js
@@ -1,28 +1,106 @@
-// poweron button
-document.querySelector("button#on").addEventListener("click", () => {
-  let buttons = document.querySelectorAll("button");
-  buttons.forEach(button => {
-    button.removeAttribute("disabled");
-  });
-  console.log("Cal on");
-});
+const calculator = {
+  displayValue: "0",
+  firstOperand: null,
+  isSecondOperand: false,
+  operator: null
+};
 
-document.querySelector("button#off").addEventListener("click", () => {
-  let buttons = document.querySelectorAll("button");
-  buttons.forEach(button => {
-    button.setAttribute("disabled", "disabled");
-  });
-  console.log("Calc off");
-  document.querySelector("button#on").removeAttribute("disabled");
-});
+function inputDigit(digit) {
+  const { displayValue, isSecondOperand } = calculator;
 
-document.querySelector(".number").addEventListener("click", () => {
-  button = document.querySelector("button").value;
-  document.querySelector("#input").textContent = button;
+  if (isSecondOperand === true) {
+    calculator.displayValue = digit;
+    calculator.isSecondOperand = false;
+  } else {
+    calculator.displayValue =
+      displayValue === "0" ? digit : displayValue + digit;
+  }
+}
+
+function inputDecimal(dot) {
+  if (calculator.isSecondOperand === true) return;
+
+  // If the `displayValue` does not contain a decimal point
+  if (!calculator.displayValue.includes(dot)) {
+    // Append the decimal point
+    calculator.displayValue += dot;
+  }
+}
+
+function handleOperator(nextOperator) {
+  const { firstOperand, displayValue, operator } = calculator;
+  const inputValue = parseFloat(displayValue);
+
+  if (operator && calculator.isSecondOperand) {
+    calculator.operator = nextOperator;
+    return;
+  }
+
+  if (firstOperand == null) {
+    calculator.firstOperand = inputValue;
+  } else if (operator) {
+    const currentValue = firstOperand || 0;
+    const result = performCalculation[operator](currentValue, inputValue);
+
+    calculator.displayValue = String(result);
+    calculator.firstOperand = result;
+  }
+
+  calculator.isSecondOperand = true;
+  calculator.operator = nextOperator;
+}
+
+const performCalculation = {
+  "/": (firstOperand, secondOperand) => firstOperand / secondOperand,
+
+  "*": (firstOperand, secondOperand) => firstOperand * secondOperand,
+
+  "+": (firstOperand, secondOperand) => firstOperand + secondOperand,
+
+  "-": (firstOperand, secondOperand) => firstOperand - secondOperand,
+
+  "=": (firstOperand, secondOperand) => secondOperand
+};
+
+function resetCalculator() {
+  calculator.displayValue = "0";
+  calculator.firstOperand = null;
+  calculator.isSecondOperand = false;
+  calculator.operator = null;
+}
+
+function updateDisplay() {
+  const display = document.querySelector("#screen");
+  display.value = calculator.displayValue;
+}
+
+updateDisplay();
+
+const keys = document.querySelector(".calculator-keys");
+keys.addEventListener("click", event => {
+  const { target } = event;
+  if (!target.matches("button")) {
+    return;
+  }
+
+  if (target.classList.contains("operator")) {
+    handleOperator(target.value);
+    updateDisplay();
+    return;
+  }
+
+  if (target.classList.contains("decimal")) {
+    inputDecimal(target.value);
+    updateDisplay();
+    return;
+  }
+
+  if (target.classList.contains("ac")) {
+    resetCalculator();
+    updateDisplay();
+    return;
+  }
+
+  inputDigit(target.value);
+  updateDisplay();
 });
-// function (elem, index) { if (elem[key] === value) {
-//   ret.push(that[index]); } }); return ret.length < 2 ? ret[0] : ret; }; var bar =
-//   myArray.grep("id","45"); const populateCurrencies = () => { var opt =
-//   document.querySelector('select'); return currencies.forEach(){ var option =
-//   document.createElement("option"); option.textContent = currencies.name;
-//   option.value =currencies.id; opt.appendChild(option);
